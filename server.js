c13a03e838c372ac7260b914488879ebc26a4ba5
devpola <sonamoo9771@gmail.com>
2021-03-07 00:14:46 +0900

    Add post communication source

--- server.js
+++ server.js
@@ -35,6 +35,63 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+//before test 제출 (POST)
+app.post('/before', (req, res) => {
+    //답안지 받아옴(json으로 받아오면 될 듯!)
+
+    //채점
+    var result = req.body.score;
+    console.log(result);
+
+    //'before' cookie 채첨된 값으로 생성
+    res.cookie('before', result);
+    res.json(result);
+});
+
+//after test 제출 (POST)
+app.post('/after', (req, res) => {
+    //답안지 받아옴
+
+    //채점
+    var result = req.body.score;
+
+    //update db test user count(1씩 증가)
+    const userCount = db.collection("test").doc("user");
+    userCount.update({
+        count: admin.firestore.FieldValue.increment(1)
+    });
+
+    //before, after test 점수 저장
+    var beforeScore = req.cookies.before;
+    var afterScore;
+
+    if(beforeScore === undefined){  //before test에 임하지 않고, after test에만 참여하려 했을 때
+        res.error();
+    } else if(beforeScore === "done already"){  //이미 두 테스트 모두 제출한 경우
+        res.json("done already");
+    } else {    
+        afterScore = result;
+    }
+
+    console.log(beforeScore);
+    console.log(afterScore);
+
+    //db test 상승 여부 count update
+    const scoreIncrease = db.collection("test").doc("score");
+
+    //점수의 상승이 있었을 때만 1씩 증가
+    if(afterScore - beforeScore > 0){
+        scoreIncrease.update({
+            increase: admin.firestore.FieldValue.increment(1)
+        });
+    }
+
+    //after test 제출 후, before cookie에 특정 값 주어
+    //두 테스트 모두 통과한 user와 before test에 임하지 않고 after test를 제출하려는 user 구분
+    res.cookie('before', "done already");
+    res.json(result);
+});
+
 app.listen(3000, () => {
     console.log("app is running on port 3000");
 });
