d665aa2eb8270c302a8a7c5de52066a69e05af95
unknown <jwy06048@gmail.com>
2021-03-23 01:55:55 +0900

    Modify before test done

--- public/script/useCookies.js
+++ public/script/useCookies.js
@@ -12,15 +12,31 @@ function checkSubmitTest() {
 }
 
 
-
 function afterSubmitPageTransition() {
     let cookies = Cookies.get();
-    if(cookies.curPage) {
-        gsap.to('#root', { duration: 0, scrollTo: { y: cookies.curPage, offsetY: 0 } } );
+    if (cookies.curPage) {
+        gsap.to('#root', { duration: 0, scrollTo: { y: cookies.curPage, offsetY: 0 } });
     } else {
-        gsap.to('#root', { duration: 0, scrollTo: { y: '#main_top', offsetY: 0 } } );
+        gsap.to('#root', { duration: 0, scrollTo: { y: '#main_top', offsetY: 0 } });
     }
 }
 
-
-
+function checkTestDone() {
+    let ownScore = document.querySelectorAll('.own-score.chart-comment');
+    let lastChartComment = document.querySelector('#last-score-comment');
+    let notYet = document.querySelector('.not-yet-test');
+    let cookies = Cookies.get();
+    if (cookies.test === 'done') {
+        ownScore.forEach(ownScore => {
+            $(ownScore).show();
+        })
+        $(lastChartComment).show();
+        $(notYet).hide();
+    } else {
+        ownScore.forEach(ownScore => {
+            $(ownScore).hide();
+        })
+        $(lastChartComment).hide();
+        $(notYet).show();
+    }
+}
