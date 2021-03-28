5304d456b3450137845351ffbb163fc517125caf
unknown <jwy06048@gmail.com>
2021-03-29 01:57:00 +0900

    Modify main top

--- public/script/pageTransition.js
+++ public/script/pageTransition.js
@@ -47,6 +47,8 @@ btnOptions.forEach(({ btnSelector, pageSelector, offsetY }) => {
 const page = [".top", "#main_middle", "#main_bottom", ".before_test", ".after_test"];
 
 page.forEach(p => {
-    if (p === ".top") document.querySelector(p).style.height = "auto";
-    else document.querySelector(p).style.height = window.innerHeight + 'px';
+    if (p === ".top") {
+        if (document.querySelector(".top").clientHeight > window.height)
+            document.querySelector(p).style.height = "auto";
+    } else document.querySelector(p).style.height = window.innerHeight + 'px';
 })
